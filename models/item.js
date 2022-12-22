017e3a7b6f999f477434d9bc78a8de359e388d2b
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2022-12-22 09:07:47 -0500

    initial commit

---
+++ models/item.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+// opitonal shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+    comments: String,
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+},
+{
+    timestamps: true
+})
+
+const itemSchema = new Schema({
+    name: String,
+    brand: String,
+    category: String,
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},
+    available: {type: Boolean, default: true},
+    dueDate: {type: Number, default: function() {
+            return new Date()
+        }},
+    comments: [commentSchema]
+}, {
+    timestamps: true
+})
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Movie', movieSchema)
